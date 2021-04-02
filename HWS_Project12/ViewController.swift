b605a5bfa477af5121e14627e1e9c709e4cb4e53
nofprog <k.jin3217@gmail.com>
2021-04-02 23:19:49 +0900

    UserDefaults

--- HWS_Project12/ViewController.swift
+++ HWS_Project12/ViewController.swift
@@ -11,9 +11,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let defaults = UserDefaults.standard
+        
+        defaults.set(25, forKey: "Age")
+        defaults.set(true, forKey: "UseFaceID")
+        defaults.set(CGFloat.pi, forKey: "Pi")
+
+        
+        defaults.set("Nof", forKey: "Name")
+        defaults.set(Date(), forKey: "LastRun")
+        
+        let array = ["Hello", "World"]
+        defaults.set(array, forKey: "SavedArray")
+        
+        let dict = ["Name": "Nof", "Country": "Japan"]
+        defaults.set(dict, forKey: "SavedDictionary")
+        
+        /*
+         integer(forKey:) returns an integer if the key existed, or 0 if not.
+         bool(forKey:) returns a boolean if the key existed, or false if not.
+         float(forKey:) returns a float if the key existed, or 0.0 if not.
+         double(forKey:) returns a double if the key existed, or 0.0 if not.
+         object(forKey:) returns Any? so you need to conditionally typecast it to your data type.
+
+         */
+        
     }
 
 
 }
+
 
